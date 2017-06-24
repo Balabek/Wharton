69208cbdf41c7e3a72d3314d52da3a1c6b786527
Balabek <vonbabayan@gmail.com>
2017-06-25 01:37:04 +0300

    Initial (Desktop Only) version (no footer yet)

--- js/wharton.js
+++ js/wharton.js
@@ -2,26 +2,48 @@
  * Created by krm on 22/06/2017.
  */
 
-// $(document).ready(function(){
-//
-//
-//     // var upperButton = $('#collapser');
-//     // var lowerButton = $('#de_collapser');
-//     //
-//     // $(upperButton).on('click',function() {
-//     //
-//     //     $(this).css('display','none');
-//     //
-//     // });
-//     //
-//     // $(lowerButton).on('click',function() {
-//     //
-//     //     $(upperButton).css('display','block');
-//     //
-//     // });
-//
-//
-//
-//
-//
-// });
+
+
+$(document).ready(function(){
+
+    // Buttons behavior for 'Team'
+    var upperButton = $('#collapser');
+    var lowerButton = $('#de_collapser');
+
+    $(upperButton).on('click',function() {
+
+        $(this).css('display','none');
+
+    });
+
+    $(lowerButton).on('click',function() {
+
+        $(upperButton).css('display','block');
+
+    });
+
+
+
+    // Buttons behavior for 'Brands'
+    var upperButton2 = $('#collapser_2');
+    var lowerButton2 = $('#de_collapser_2');
+
+    $(upperButton2).on('click',function() {
+
+        $('.all_brandz_button_holder').css('display','none');
+
+    });
+
+    $(lowerButton2).on('click',function() {
+
+        $('.all_brandz_button_holder').css('display','block');
+
+    });
+
+
+
+
+
+
+
+});
