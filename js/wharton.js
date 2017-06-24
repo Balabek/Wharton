5b8598ad663905f05f5575c9632b75b8a984d592
Balabek <vonbabayan@gmail.com>
2017-06-24 21:33:39 +0300

    Initial fetus

---
+++ js/wharton.js
@@ -0,0 +1,27 @@
+/**
+ * Created by krm on 22/06/2017.
+ */
+
+// $(document).ready(function(){
+//
+//
+//     // var upperButton = $('#collapser');
+//     // var lowerButton = $('#de_collapser');
+//     //
+//     // $(upperButton).on('click',function() {
+//     //
+//     //     $(this).css('display','none');
+//     //
+//     // });
+//     //
+//     // $(lowerButton).on('click',function() {
+//     //
+//     //     $(upperButton).css('display','block');
+//     //
+//     // });
+//
+//
+//
+//
+//
+// });
